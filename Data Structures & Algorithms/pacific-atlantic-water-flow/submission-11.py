972b58dd352e3e9a396bbc94085de1095d6224ed
Will Chou <92957081+weichi2002@users.noreply.github.com>
2026-06-02 08:50:16 -0400

    Add: pacific-atlantic-water-flow - submission-11

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-11.py
@@ -0,0 +1,44 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        
+        #reverse the exploration. We flow from less to equal or more from the edge
+        #use set to represent list of coordinates, and find intersection
+
+        pac, atl = set(), set()
+
+        ROWS = len(heights)
+        COLS = len(heights[0])
+
+        def dfs(r, c, visit, prev):
+
+            if (
+                r not in range(ROWS) or
+                c not in range(COLS) or
+                (r, c) in visit or
+                heights[r][c] < prev
+            ):
+                return 
+            
+            visit.add((r,c))
+            dfs(r+1, c, visit, heights[r][c])
+            dfs(r-1, c, visit, heights[r][c])
+            dfs(r, c+1, visit, heights[r][c])
+            dfs(r, c-1, visit, heights[r][c])
+
+        #mark all of 
+        for r in range(ROWS):
+            dfs(r, 0, pac, heights[r][0])
+            dfs(r, COLS-1, atl, heights[r][COLS-1])
+
+        for c in range(COLS):
+            dfs(0, c, pac, heights[0][c])
+            dfs(ROWS-1, c, atl, heights[ROWS-1][c])
+        
+        return list(pac & atl)
+
+
+
+
+
+
+
